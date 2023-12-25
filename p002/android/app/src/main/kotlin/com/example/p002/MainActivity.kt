a5d06750598fbcc9212162d2d842ee722c018af5
papesaniebendao <ndao.pape-saniebe@ugb.edu.sn>
2023-12-25 15:48:39 +0100

    troixieme commit

--- p002/android/app/src/main/kotlin/com/example/p002/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.p002
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- p003/android/app/src/main/kotlin/com/example/p003/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.p003
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
